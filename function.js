e87060d39ab150a926826054fba332235903ceb1
maheshwariredd <gunreddyakihlreddy555@gmail.com>
2025-01-09 11:50:53 +0530

    message is sending to git hub

---
+++ function.js
@@ -0,0 +1,24 @@
+//let a;
+//console.log(a);
+
+//function demo (){
+    //console.log("functon with out parameter");
+
+//}
+//demo();
+
+//function with parameters
+
+//function userdetails (name , age , city){
+   // console.log(name);
+    //console.log(age);
+   // console.log(city);
+//}
+//userdetails ("mahi" , 20)
+
+//iife
+(function (){
+    console.log("iifs");
+
+})
+();
